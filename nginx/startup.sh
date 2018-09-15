39418490fd34d4af499a06daf70907fae606ca3e
Nathan Friend <hello@nathanfriend.io>
2018-09-15 12:47:20 -0300

    making startup script smarter

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -3,17 +3,28 @@
 # Start a temporary nginx instance
 nginx -c /etc/nginx/nginx.startup.conf
 
-# Get SSL certs for all the domains we serve
-# certbot certonly --webroot -w /usr/share/nginx/html \ 
-#     -d nathanfriend.io -d www.nathanfriend.io -d nathanfriend.com -d www.nathanfriend.com \
-#     -d dev.nathanfriend.io -d dev.nathanfriend.com \
-#     -d bethany.and.nathanfriend.io -d www.bethany.and.nathanfriend.io -d bethany.and.nathanfriend.com -d www.bethany.and.nathanfriend.com \
-#     -n --agree-tos --email hello@nathanfriend.io
+# Test if we already have existing certificates. If we don't,
+# get new ones.  Otherwise, just try and renew the existing ones.
+if [ -d /etc/letsencrypt/live ] && [ ! -z "$(ls -A /etc/letsencrypt/live)" ];
+then 
+    echo "Existing certificates were found... running renewal"
 
-certbot certonly --webroot -w /usr/share/nginx/html \
+    certbot renew
+else
+    echo "No existing certificates found... requesting new ones from Let's Encrypt"
+
+    certbot certonly --test-cert --webroot -w /usr/share/nginx/html \
     -d asdf.nathanfriend.io -d www.asdf.nathanfriend.io \
     -d blah.nathanfriend.io -d www.blah.nathanfriend.io \
     -n --agree-tos --email hello@nathanfriend.io
+
+    # Get SSL certs for all the domains we serve
+    # certbot certonly --webroot -w /usr/share/nginx/html \ 
+    #     -d nathanfriend.io -d www.nathanfriend.io -d nathanfriend.com -d www.nathanfriend.com \
+    #     -d dev.nathanfriend.io -d dev.nathanfriend.com \
+    #     -d bethany.and.nathanfriend.io -d www.bethany.and.nathanfriend.io -d bethany.and.nathanfriend.com -d www.bethany.and.nathanfriend.com \
+    #     -n --agree-tos --email hello@nathanfriend.io
+fi
 
 # Stop the temporary instance
 nginx -s stop
@@ -22,5 +33,5 @@ nginx -s stop
 sleep 1
 
 # Start the real nginx instance
-nginx -g "daemon off"
+nginx -g "daemon off;"
 
