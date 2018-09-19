24392148ca803d321f41c9de95bcd942cc0fd502
Nathan Friend <hello@nathanfriend.io>
2018-09-19 19:24:32 -0300

    get real certs

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -23,17 +23,17 @@ else
     else
         echo "No existing certificates found... requesting new ones from Let's Encrypt"
 
-        certbot certonly --test-cert --webroot -w /usr/share/nginx/html \
-        -d asdf.nathanfriend.io -d www.asdf.nathanfriend.io \
-        -d blah.nathanfriend.io -d www.blah.nathanfriend.io \
-        -n --agree-tos --email hello@nathanfriend.io
+        # certbot certonly --test-cert --webroot -w /usr/share/nginx/html \
+        # -d asdf.nathanfriend.io -d www.asdf.nathanfriend.io \
+        # -d blah.nathanfriend.io -d www.blah.nathanfriend.io \
+        # -n --agree-tos --email hello@nathanfriend.io
 
         # Get SSL certs for all the domains we serve
-        # certbot certonly --webroot -w /usr/share/nginx/html \ 
-        #     -d nathanfriend.io -d www.nathanfriend.io -d nathanfriend.com -d www.nathanfriend.com \
-        #     -d dev.nathanfriend.io -d dev.nathanfriend.com \
-        #     -d bethany.and.nathanfriend.io -d www.bethany.and.nathanfriend.io -d bethany.and.nathanfriend.com -d www.bethany.and.nathanfriend.com \
-        #     -n --agree-tos --email hello@nathanfriend.io
+        certbot certonly --webroot -w /usr/share/nginx/html \ 
+            -d nathanfriend.io -d www.nathanfriend.io -d nathanfriend.com -d www.nathanfriend.com \
+            -d dev.nathanfriend.io -d dev.nathanfriend.com \
+            -d bethany.and.nathanfriend.io -d www.bethany.and.nathanfriend.io -d bethany.and.nathanfriend.com -d www.bethany.and.nathanfriend.com \
+            -n --agree-tos --email hello@nathanfriend.io
     fi
 
 fi
