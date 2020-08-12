f1c97de2f345ced6ecef9fe08e67afeed1974a45
Nathan Friend <nathan@gitlab.com>
2020-08-11 21:31:54 -0400

    Begin working on new deploy process

---
+++ scripts/deploy.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+##
+## Run ssh-agent (inside the build environment)
+##
+eval $(ssh-agent -s)
+
+##
+## Add the SSH key stored in SSH_PRIVATE_KEY variable to the agent store
+## We're using tr to fix line endings which makes ed25519 keys work
+## without extra base64 encoding.
+## https://gitlab.com/gitlab-examples/ssh-private-key/issues/1#note_48526556
+##
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add -
+
+##
+## Create the SSH directory and give it the right permissions
+##
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+##
+## Verify the SSH host keys.
+## See https://docs.gitlab.com/ee/ci/ssh_keys/#verifying-the-ssh-host-keys
+##
+echo "$SSH_KNOWN_HOSTS" >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+
+ssh ${SERVER_USER}@${SERVER_ADDR} <<EOF
+
+  ##
+  ## Clone the website-3.0-docker repo locally, if necessary
+  ##
+  if [[ ! -d website-3.0-docker ]]; then
+    cd /home/${SERVER_USER}
+    git clone https://gitlab.com/nfriend/website-3.0-docker.git
+  fi
+
+  ##
+  ## Update the local repo
+  ##
+  git checkout master
+  git fetch
+  git reset --hard origin/master
+
+  ##
+  ## Take down any current services
+  ##
+  cd /home/${SERVER_USER}/website-3.0-docker
+  docker-compose down
+
+  ##
+  ## Install all dependencies
+  ##
+  cd /home/${SERVER_USER}/website-3.0-docker
+  npm install
+
+  ##
+  ## Bring up all the services
+  ##
+  cd /home/${SERVER_USER}/website-3.0-docker
+  docker-compose up -d
+EOF
---
+++ scripts/init-lets-encrypt.template.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Based on the script found in
+# https://medium.com/@pentacent/nginx-and-lets-encrypt-with-docker-in-less-than-5-minutes-b4b8a60d3a71
+
+domains=(
+  nathanfriend.io
+  nathanfriend.com
+  www.nathanfriend.io
+  www.nathanfriend.com
+  dev.nathanfriend.io
+  dev.nathanfriend.com
+  bethany.and.nathanfriend.io
+  bethany.and.nathanfriend.com
+  www.bethany.and.nathanfriend.com
+  www.bethany.and.nathanfriend.io
+)
+email="contact@nathanfriend.io"
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+rsa_key_size=4096
+
+echo "### Deleting dummy certificate for $domains ..."
+docker-compose -f docker-compose.yml \
+               -f docker-compose.production.yml \
+               run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+docker-compose -f docker-compose.yml \
+               -f docker-compose.production.yml \
+               run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+echo
+
+echo "### Reloading nginx ..."
+docker-compose -f docker-compose.yml \
+               -f docker-compose.production.yml \
+               exec nginx nginx -s reload
