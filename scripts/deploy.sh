8894ed49ef929512774ab8bdf57349e2e39677a0
Nathan Friend <nathan@gitlab.com>
2020-08-13 09:39:32 -0400

    Correct file name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,34 +28,24 @@ chmod 644 ~/.ssh/known_hosts
 
 ssh -p ${SSH_PORT} ${SERVER_USER}@${SERVER_ADDR} <<EOF
 
-  ##
-  ## Clone the website-3.0-docker repo locally, if necessary
-  ##
+  echo "Cloning the website-3.0-docker repo locally, (if necessary)..."
   if [[ ! -d website-3.0-docker ]]; then
     cd /home/${SERVER_USER}
     git clone https://gitlab.com/nfriend/website-3.0-docker.git
   fi
 
-  ##
-  ## Navigate to the local repo
-  ##
+  echo "Navigating to the local repo..."
   cd /home/${SERVER_USER}/website-3.0-docker
 
-  ##
-  ## Update the local repo
-  ##
+  echo "Updating the local repo..."
   git checkout master
   git fetch
   git reset --hard origin/master
 
-  ##
-  ## Take down any current services
-  ##
+  echo "Taking down any current services..."
   docker-compose -f docker-compose.yml -f docker-compose.production.yml down
 
-  ##
-  ## Copy temporary SSL certificates
-  ##
+  echo "Coping temporary SSL certificates..."
   mkdir -p /home/${SERVER_USER}/website-3.0-docker/certbot/www
   mkdir -p /home/${SERVER_USER}/website-3.0-docker/certbot/conf/live/${SERVER_ADDR}
   rsync --archive \
@@ -71,13 +61,9 @@ ssh -p ${SSH_PORT} ${SERVER_USER}@${SERVER_ADDR} <<EOF
         nginx/deployed/temp.key.pem \
         /home/${SERVER_USER}/website-3.0-docker/certbot/conf/live/${SERVER_ADDR}/privkey.pem
 
-  ##
-  ## Install all dependencies
-  ##
+  echo "Installing all dependencies.."
   npm install
 
-  ##
-  ## Bring up all the services
-  ##
+  echo "Bringing up all the services..."
   docker-compose -f docker-compose.yml -f docker-compose.production.yml up -d
 EOF
