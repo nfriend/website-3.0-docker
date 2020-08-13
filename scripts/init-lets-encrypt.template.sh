64cbc7bcbc9d67b62db1deef40a2060bc398fc99
Nathan Friend <nathan@gitlab.com>
2020-08-12 20:43:22 -0400

    :facepalm:

--- scripts/init-lets-encrypt.template.sh
+++ scripts/init-lets-encrypt.template.sh
@@ -23,8 +23,8 @@ staging=1 # Set to 1 if you're testing your setup to avoid hitting request limit
 rsa_key_size=4096
 
 echo '### Creating required directories'
-mkdir -p /home/gitlabci/dannp2/certbot/www
-mkdir -p /home/gitlabci/dannp2/certbot/conf/live/$domains
+mkdir -p /home/gitlabci/website-3.0-docker/certbot/www
+mkdir -p /home/gitlabci/website-3.0-docker/certbot/conf/live/$domains
 
 echo "### Deleting dummy certificate for $domains ..."
 docker-compose -f docker-compose.yml \
